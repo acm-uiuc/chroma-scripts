afefd3b9729fd2fe5d4d764a4b06c3a07abec8ed
Kevin Lange <kevin.lange@dakko.us>
2012-03-14 18:19:27 -0500

    White

--- animations/single-color/main.py
+++ animations/single-color/main.py
@@ -6,7 +6,13 @@ from osc import ColorsOut
 if __name__ == "__main__":
     import time
     out = ColorsOut()
+    #r = 127.0
+    #g = 101.0
+    #b = 30.0
+    r = 255.0
+    g = 255.0
+    b = 255.0
     while True:
-        pix = [(4.0 * 127.0, 4.0 * 101.0, 4.0 * 30.0)] * 24
+        pix = [(4.0 * r, 4.0 * g, 4.0 * b)] * 24
         out.write(pix)
         time.sleep(0.2)
