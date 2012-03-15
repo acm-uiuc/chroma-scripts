fdc9ce599fc7ee5cac500aaafd5dd07a0ccb1338
Kevin Lange <kevin.lange@dakko.us>
2012-03-15 18:00:31 -0500

    Un-white-balance 'plasma'.

--- animations/plasma/main.py
+++ animations/plasma/main.py
@@ -53,6 +53,6 @@ if __name__ == "__main__":
                 h, s, v = board[(i, j)]
                 r, g, b = hsv_to_rgb(h * 360., s, v)
                 # print board[(i, j)],r, g, b
-                pix.append((r * 1.5, g * 4., b * 1.5))
+                pix.append((r * 4., g * 4., b * 4.))
         out.write(pix)
         time.sleep(1/30.)
