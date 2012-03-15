50cedbe1824265caa1d0e76debd53f14d7f89dd6
Kevin Lange <kevin.lange@dakko.us>
2012-03-15 17:35:59 -0500

    Don't white balance the solid white test

--- animations/single-color/main.py
+++ animations/single-color/main.py
@@ -6,12 +6,12 @@ from oscapi import ColorsOut
 if __name__ == "__main__":
     import time
     out = ColorsOut()
-    #r = 127.0
-    #g = 101.0
-    #b = 30.0
-    r = 60.0
+    r = 255.0
     g = 255.0
-    b = 70.0
+    b = 255.0
+    #r = 60.0
+    #g = 255.0
+    #b = 70.0
     while True:
         pix = [(4.0 * r, 4.0 * g, 4.0 * b)] * 24
         out.write(pix)
