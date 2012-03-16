b818e25282160a7c984471cd1fd0ddc85bde81a0
Kevin Lange <kevin.lange@dakko.us>
2012-03-15 19:42:21 -0500

    US flag demo

---
+++ animations/flag/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys, random
+sys.path.append("./osc")
+from animations import FadeAnimation 
+
+red   = (1023.0,0.0,0.0)
+white = (0.0,0.0,0.0)
+blue  = (0.0,1023.0,0.0)
+
+flag = [blue, blue, red,   red,
+        blue, blue, white, white,
+        red,  red,  red,   red,
+        white, white, white, white,
+        red, red, red, red,
+        white, white, white, white]
+
+if __name__ == "__main__":
+    import time
+
+    out = FadeAnimation()
+    out.start()
+    while True:
+        out.write(flag)
+        time.sleep(1.0)
+        n += direction
+
