ef1b111389b9437e5db61e097f4b78ed29a3c890
mnewton <mnewton@mnewton-XPS-L401X>
2012-03-16 20:25:12 -0500

    added new animation spiral and added more frames to boxy

--- animations/boxy/main.py
+++ animations/boxy/main.py
@@ -4,6 +4,8 @@ from oscapi import ColorsOut
 import random
 import math
 import time
+from animations import FadeAnimation 
+
 
 #todo: use smoothe animations, reverse template
 #author: Harsh Singh
@@ -19,7 +21,9 @@ bgColor=(0.0,0.0,0.0)
 #6,7
 template = [[0,1,4,5],[2,3,6,7],[8,9,12,13],[10,11,14,15],[16,17,20,21],[18,19,22,23],[0,1,2,3,4,7,8,11,12,13,14,15],[5,6,9,10],]
 #inuse = [False] * 6
-out = ColorsOut()
+out = FadeAnimation()
+out.start()
+layout = [bgColor]*24
 
 
 def repeat(**options):
@@ -81,18 +85,92 @@ def frame3():
         i+=1
         #printlayout(layout)
         
+# spin colors at 5,6,9,10 at .1 second interval
+def frame3():
+    rand = random.sample(nice_pixels,5)
+    layout = [rand[4]]*24
+    #printlayout(layout)
+    #out.write(layout)
+    #time.sleep(0.4)  
+    i = 0  
+    for j in range(len(template[i])):
+
+        #print "i= "+str(i)+" j ="+str(j)
+        layout[template[7][j]] = rand[i]
+        out.write(layout)
+        time.sleep(0.1)
+        i+=1
+        #printlayout(layout)
+
+#spiral function from 0 to 9
+def frame4():
+    path = [0,1,2,3,7,11,15,14,13,12,8,4,5,6,10,9]
+    
+    rand = random.sample(nice_pixels,4)
+    for i in range(len(path)):
+        layout[path[i]] = rand[0]
+        if (i != 0):
+            layout[path[i-1]] = rand[1]
+        # else:
+        #     layout[path[len(path)-1]] = rand[1]
+        printlayout(layout)    
+        out.write(layout)
+        time.sleep(0.05)
+    for i in range(len(path)):
+        layout[path[len(path)-1-i]] = rand[2]
+        if (i != 0):
+            layout[path[len(path)-i]] = rand[3]
+        # else:
+        #     layout[path[len(path)-1]] = rand[1]
+        printlayout(layout)    
+        out.write(layout)
+        time.sleep(0.05)
+
+def color_picker():
+    r = random.random()*1000
+    b, g = (0., 0.)
+    while True:
+        if (b > 1000):
+            r = random.random()*1000
+            b = 0
+            g = random.random()*1000
+        if (g > 1000):
+            r = random.random()*1000
+            b = random.random()*1000
+            g = 0
+        if (r > 1000):
+            g = random.random()*1000
+            b = random.random()*1000
+            r = 0
+
+        r+= 10.
+        b+= 10.
+        g+= 10.
+        layout = [(r,g,b)]*24
+        print (r,g,b)
+        out.write(layout)
+        time.sleep(.01)
+
+
+# def fadecolor(layout):
+#     #todo
+
 
 
 def printlayout(layout):
     for i in range(0,5):
         print (str(layout[i*4+0])+ " " +str(layout[i*4+1])+ " " +str(layout[i*4+2])+ " " +str(layout[i*4+3]))
-    print 
+    print
 
 if __name__ == "__main__":
-    #frame2()
+    #frame4()
+    #color_picker()
+
     while True:
+
         frame1()
         frame2()
         frame3()
-        frame2()
+        #frame2()
+        frame4()
     #print "called"
---
+++ animations/spirals/main.py
@@ -0,0 +1,72 @@
+import sys
+sys.path.append("./osc")
+import random
+import time
+from animations import FadeAnimation 
+
+nice_pixels = [
+    (1023.0,0.0,0.0),
+    (0.0,1023.0,0.0),
+    (0.0,0.0,1023.0),
+    (823.0,823.0,0.0),
+    (823.0,0.0,823.0),
+    (0.0,823.0,823.0),
+]
+template = [[0,1,4,5],[2,3,6,7],[8,9,12,13],[10,11,14,15],[16,17,20,21],[18,19,22,23],[0,1,2,3,4,7,8,11,12,13,14,15],[5,6,9,10],]
+bgColor=(0.0,0.0,0.0)
+out = FadeAnimation()
+out.start()
+layout = [bgColor]*24
+
+"""
+Spirals! Start from 0-3,3-15,15-12,12-4,4-6,6-10,10-9, then do it in reverse
+"""
+def spirals():
+    path = [0,1,2,3,7,11,15,14,13,12,8,4,5,6,10,9]
+    
+    rand = random.sample(nice_pixels,4)
+    for i in range(len(path)):
+        layout[path[i]] = rand[0]
+        #try commenting out these if statement and see if you like it better
+        if (i != 0):
+            layout[path[i-1]] = rand[1]
+        out.write(layout)
+        time.sleep(0.05)
+    for i in range(len(path)):
+        layout[path[len(path)-1-i]] = rand[2]
+        #try commenting out these if statement and see if you like it better
+        if (i != 0):
+            layout[path[len(path)-i]] = rand[3]
+        out.write(layout)
+        time.sleep(0.05)
+"""
+Make the center box blink and fade out.
+"""
+
+def blinky():
+    layout = [bgColor]*24
+
+    rand = random.sample(nice_pixels,2)
+    for i in template[len(template)-2]:
+        layout[i] = rand[0]
+    out.write(layout)
+    time.sleep(0.3)
+    for i in template[len(template)-1]:
+        layout[i] = rand[1]    
+    out.write(layout)
+    time.sleep(0.3)
+    temp = layout
+    layout = [bgColor]*24
+    #recycle code later
+    # for i in range(3):
+    #     if (i%2 == 0):
+    #         layout = [bgColor]*24
+    #     else :
+    #         layout = temp
+    #     out.write(layout)
+    #     time.sleep(0.1)
+
+if __name__ == "__main__":
+    while True:
+        spirals()
+        blinky()
