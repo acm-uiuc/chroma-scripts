e598350cfb1860b946db50bcc227f9b0051fa827
Kevin Lange <kevin.lange@dakko.us>
2012-03-17 22:30:18 -0500

    New animation: Rain

---
+++ animations/rain/main.py
@@ -0,0 +1,21 @@
+import sys, random
+sys.path.append("./osc")
+from oscapi import ColorsOut
+
+falloffRate = 20.0
+
+if __name__ == "__main__":
+    import time
+    out = ColorsOut()
+    pix = [(0.0,0.0,0.0)] * 24
+    while True:
+        # Make some rain
+        if random.randint(0,5) < 3:
+            pix[random.randint(0,23)] = (0.0,0.0,1023.0)
+        out.write(pix)
+        for i in xrange(24):
+            if pix[i][2] > 0.0:
+                pix[i] = (0.0,0.0,pix[i][2] - falloffRate)
+            else:
+                pix[i] = (0.0,0.0,0.0)
+        time.sleep(0.02)
