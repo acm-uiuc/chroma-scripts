992690b63743ac39301860b5f514ca6adb3464dd
Robert P <robert.m.pieta@gmail.com>
2012-03-28 19:08:08 -0500

    more furnace updates

--- animations/furnace/main.py
+++ animations/furnace/main.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
     import time
     out = ColorsOut()
     pix = [(0.0,0.0,0.0)] * 24
-    totalFrames = 130
-    frames = totalFrames
+    runInAnimation = True
+    smoothnessRatio = 200
+    sleepTime = .01
     while True:
         # Make some rain
         #    if random.randint(0,5) < 3:
@@ -21,17 +22,17 @@ if __name__ == "__main__":
         #       pix[i] = (pix[i][0] - falloffRate, pix[i][1] - falloffRate,pix[i][2] - falloffRate)
         #else:
         #       pix[i] = (0.0,0.0,0.0)
-        if frames > 0:
+        if runInAnimation:
             for i in xrange(12):
-                pix[random.randint(0,23)] = (pix[i][0] + 1023/70, 0.0, 0.0)
+                pix[random.randint(0,23)] = (pix[i][0] + 1023/smoothnessRatio, 0.0, 0.0)
             out.write(pix)
-            frames = frames - 1
-            time.sleep(.04)
+            if pix[i][0] > 900:
+                    runInAnimation = False
+            time.sleep(sleepTime)
         else:
             for i in xrange(12):
-                pix[random.randint(0,23)] = (pix[i][0] - 1023/70, 0.0, 0.0)
+                pix[random.randint(0,23)] = (pix[i][0] - 1023/smoothnessRatio, 0.0, 0.0)
             out.write(pix)
-            frames = frames - 1
-            if frames < -totalFrames:
-                frames = totalFrames
-            time.sleep(.04)
+            if pix[i][0] < 100.0:
+                runInAnimation = True
+            time.sleep(sleepTime)
