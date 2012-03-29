e5a70e06f8e20d18d3eb496c96b2ce1a443cb77a
Jake McGinty <mcginty@illinois.edu>
2012-03-29 00:11:36 -0500

    Fixed argument parsing and usage output.

--- run.py
+++ run.py
@@ -1,23 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import sys
 import os
+import string
 import subprocess
 import signal
 import argparse
 
-parser = argparse.ArgumentParser(description="Run a specific chrome animation script.")
-parser.add_argument('animation_name',
-    metavar='animation', 
-    type=str,
-    help='Name of the animation, specifically name of the folder in animations/.')
+parser = argparse.ArgumentParser(description="Run a specific chroma animation script.")
+parser.add_argument('animation',
+    metavar='animation',
+    help='One of these animations: '+string.join(os.listdir('animations/'),', '),
+    choices=os.listdir('animations/'))
+
 args = parser.parse_args()
-animation = args.animation_name
+
+animation = args.animation
 
 p = subprocess.Popen(['python','animations/%s/main.py'%animation])
 
 print "Running animation %s"%animation
 
-print "Press any key to end"
+print "Press enter to end"
 
 i = raw_input()
 
