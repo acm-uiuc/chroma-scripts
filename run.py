ba0848fff9cda4de2da8606703ccae0cb261dda9
Jake McGinty <mcginty@illinois.edu>
2012-03-28 20:41:15 -0500

    added better argument handling for run.py to help user actually know what to put in.

--- run.py
+++ run.py
@@ -3,8 +3,15 @@ import sys
 import os
 import subprocess
 import signal
+import argparse
 
-animation = sys.argv[1]
+parser = argparse.ArgumentParser(description="Run a specific chrome animation script.")
+parser.add_argument('animation_name',
+    metavar='animation', 
+    type=str,
+    help='Name of the animation, specifically name of the folder in animations/.')
+args = parser.parse_args()
+animation = args.animation_name
 
 p = subprocess.Popen(['python','animations/%s/main.py'%animation])
 
