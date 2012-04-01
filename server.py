932ace304e1fff056347e2548b2bf27fff649692
Robert P <robert.m.pieta@gmail.com>
2012-04-01 14:48:49 -0500

    Server updates and animation updates

--- server.py
+++ server.py
@@ -11,6 +11,30 @@ lookup = TemplateLookup(directories=['templates'])
 p = None
 current = "Off"
 
+def sortDictByValueKey(dict, valuekey):
+    sortedValueKeys = {}
+    for key in dict.iterkeys(): 
+        if(valuekey == "name"):
+            value = dict[key][valuekey] + "1"
+        else:
+            value = dict[key][valuekey] + dict[key]["name"]
+        
+        while(value in sortedValueKeys.keys()):
+            if(valuekey == "name"):
+                value = value + "1"
+            else:
+                value = value + dict[key]["name"]
+                print value
+        
+        sortedValueKeys[value] = key
+    
+    sortedKeys = []
+    for key in sorted(sortedValueKeys.iterkeys()):
+        sortedKeys.append(sortedValueKeys[key])
+    
+    return sortedKeys
+
+
 class Serv:
    @http.expose
    def index(self):
@@ -22,8 +46,11 @@ class Serv:
          data = json.load(json_data)
          aData[a] = data
          json_data.close()
+    
+      sortedKeys = sortDictByValueKey(aData, "name")
+                 
       tmpl = lookup.get_template("index.html")
-      return tmpl.render(animations=aData,current=current)
+      return tmpl.render(animations=aData,current=current,animationKeys=sortedKeys)
 
    @http.expose
    @http.tools.allow(methods=['POST'])
