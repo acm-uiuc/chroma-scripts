d47c1fc4d1e6ad97373b122cec9864ce2b674245
Reed La Botz <reedlabotz@gmail.com>
2012-04-19 17:57:45 -0500

    starting switch to liquid

--- server.py
+++ server.py
@@ -5,56 +5,9 @@ import subprocess
 import signal
 import simplejson as json
 
-from mako.template import Template
-from mako.lookup import TemplateLookup
-lookup = TemplateLookup(directories=['templates'])
-p = None
-current = "Off"
-
-def sortDictByValueKey(dict, valuekey):
-    sortedValueKeys = {}
-    for key in dict.iterkeys(): 
-        if(valuekey == "name"):
-            value = dict[key][valuekey] + "1"
-        else:
-            value = dict[key][valuekey] + dict[key]["name"]
-        
-        while(value in sortedValueKeys.keys()):
-            if(valuekey == "name"):
-                value = value + "1"
-            else:
-                value = value + dict[key]["name"]
-                print value
-        
-        sortedValueKeys[value] = key
-    
-    sortedKeys = []
-    for key in sorted(sortedValueKeys.iterkeys()):
-        sortedKeys.append(sortedValueKeys[key])
-    
-    return sortedKeys
-
-
 class Serv:
    @http.expose
-   def index(self):
-      global current
-      animations  = os.listdir('./animations/')
-      aData = {}
-      for a in animations:
-         json_data=open('animations/%s/manifest.json'%a)
-         data = json.load(json_data)
-         aData[a] = data
-         json_data.close()
-    
-      sortedKeys = sortDictByValueKey(aData, "name")
-                 
-      tmpl = lookup.get_template("index.html")
-      return tmpl.render(animations=aData,current=current,animationKeys=sortedKeys)
-
-   @http.expose
-   @http.tools.allow(methods=['POST'])
-   def set(self, animation):
+   def play(self, animation):
       global p
       global current
       if p:
@@ -70,7 +23,6 @@ class Serv:
       return "Unknown animation %s"%animation
 
    @http.expose
-   @http.tools.allow(methods=['POST'])
    def pull(self):
       p = os.popen('git pull origin master','r')
       output = ""
@@ -78,17 +30,23 @@ class Serv:
          line = p.readline()
          if not line: break
          output += line + '\n'
-      return output
+      animations  = os.listdir('./animations/')
+      aData = []
+      for a in animations:
+         json_data=open('animations/%s/manifest.json'%a)
+         data = json.load(json_data)
+         aData.append(data)
+         json_data.close()
+      ret_data = {'output':output, "animations":aData}
+      return json.dumps(ret_data)
+         
+      
 
 if __name__ == "__main__":
    config = {
-             '/static':{
-                    'tools.staticdir.on': True,
-                    'tools.staticdir.dir': os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
-             }
    }
-   
-   
+
+
    http.tree.mount(Serv(), '/', config = config)
    http.config.update( {'server.socket_host':"0.0.0.0", 'server.socket_port':8181 } )
    http.engine.signal_handler.subscribe()
