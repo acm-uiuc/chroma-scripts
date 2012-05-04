ae2e0f5bef8995cd5e731808578d719065c52b44
root <root@lab2.acm.uiuc.edu>
2012-05-04 03:56:27 -0500

    yay sitesquares

--- animations/sitesquares/main.py
+++ animations/sitesquares/main.py
@@ -11,15 +11,17 @@ if __name__ == "__main__":
     out.start()
 
     while True:
-        data = urllib2.urlopen(URL)
+        f = urllib2.urlopen(URL)
+        data = f.read()
+        data = data[1:-2]
         data = simplejson.loads(data)
         pix = [(0.0,0.0,0.0)]*24
         for i in range(24):
             item = data[i]
             color = item["color"]
-            r = int('0x'+color[0:1])
-            g = int('0x'+color[2:3])
-            b = int('0x'+color[4:5])
+            r = int('0x'+color[0:2],16)
+            g = int('0x'+color[2:4],16)
+            b = int('0x'+color[4:6],16)
             pix[i]=(r*4.0,g*4.0,b*4.0)
         out.write(pix)
         time.sleep(2.7)
