5c1f99a8a81f9f42f68b214a182c4ea97c379a93
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-05-04 21:43:40 -0500

    Updated the kwaves

--- animations/kwaves/main.py
+++ animations/kwaves/main.py
@@ -3,7 +3,7 @@ from animations import FadeAnimation
 from math import sin, radians, degrees
 from colorsys import hsv_to_rgb
 
-speed = 40.0
+speed = 2.0
 
 if __name__ == "__main__":
     import time
@@ -13,30 +13,45 @@ if __name__ == "__main__":
 
     counter = 0
 
-    green = [(0.2, 0.5, 0.2)]
-    blue = [(0.0,0.0,0.8)]
-    red = [(0.7, 0.2, 0.2)]
-    yellow = [(0.9, 0.9, 0.1)]
+    green = (1024*0.2, 1024*0.5, 1024*0.2)
 
     while True:
       pix = [(0.0,0.0,0.0)]*24
       if counter ==0:
-        pix[0:4] = green
-        counter++
+        pix[0] = green
+        pix[1] = green
+        pix[2] = green
+        pix[3] = green
+        counter += 1
       elif counter ==1:
-        pix[4:8] = green
-        counter++
+        pix[4] = green
+        pix[5] = green
+        pix[6] = green
+        pix[7] = green
+        counter += 1
       elif counter ==2:
-        pix[8:12] = green
-        counter++
+        pix[8] = green
+        pix[9] = green
+        pix[10] = green
+        pix[11] = green
+        counter += 1
       elif counter ==3:
-        pix[12:16] = green
-        counter++
+        pix[12] = green
+        pix[13] = green
+        pix[14] = green
+        pix[15] = green
+        counter += 1
       elif counter ==4:
-        pix[16:20] = green
-        counter++
+        pix[16] = green
+        pix[17] = green
+        pix[18] = green
+        pix[19] = green
+        counter += 1
       elif counter ==5:
-        pix[20:24] = green
+        pix[20] = green
+        pix[21] = green
+        pix[22] = green
+        pix[23] = green
         counter =0
       out.write(pix)
       time.sleep(1.0/speed)
