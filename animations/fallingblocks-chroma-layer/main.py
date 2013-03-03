fcd72c188273862f73442ddd07e34b6f236ea6cd
scowalt <srwalte2@illinois.edu>
2013-03-03 15:16:50 -0600

    Continued work on fallingblocks

--- animations/fallingblocks-chroma-layer/main.py
+++ animations/fallingblocks-chroma-layer/main.py
@@ -2,40 +2,81 @@ import sys
 from animations import FadeAnimation
 from simpleOSC import initOSCServer, startOSCServer, closeOSC, setOSCHandler
 import OSC
+from random import random
 
-ip = "127.0.0.1"
+DEBUG = 1
+
+ip = "127.0.0.1" # receiving osc from 
 port = 9125
-NUMBER_OF_LIGHTS = 48
-DEFAULT_COLOR = [25, 25, 25]
-canvas = DEFAULT_COLOR * NUMBER_OF_LIGHT
+NUMBER_OF_LIGHTS = 24 # currently 24, EOH 48
+DEFAULT_COLOR = [(25.0, 25.0, 25.0)] # dim white light
+canvas = DEFAULT_COLOR * NUMBER_OF_LIGHTS
+orbs = [[]] * NUMBER_OF_LIGHTS # data on the orbs on the screen
+out = FadeAnimation()
 
-def setup():
+def run():
+	# SETUP #
+	resetCanvas()
 	import time
-	initOSCServer(ip, port)
+	out.FADERATE = 8.0 # 8 second fading
+	out.start()  # start visualization
+	initOSCServer(ip, port) # start OSC server
 	
 	# OSC HANDLERS #
-	setOSCHandler('/collision', collision)
+	setOSCHandler('/entry', entry)
+	setOSCHandler('/exit', exit)
 
 	# SERVER START #
 	startOSCServer()
 	print "server is running, listening at " + str(ip) + ":" + str(port)
 
 	try:
+		clock = 0
 		while(1):
-			time.sleep(1000)
+			out.write(canvas) #write to canvas
+			time.sleep(0.1)
+			if ((clock%100 == 0)): # every 100 ticks
+				plasmaCanvas()
+			clock = (clock+1)%10000 # reset clock every 10000 ticks
 	except KeyboardInterrupt:
 		print "closing all OSC connections... and exit"
-		closeOSC() # finally close the connection before exiting or program.
-
-def collision(addr, tags, data, source):
-	print "HEY I GOT A THING!!!"
-	print "addr = " + str(addr) # /collision
-	print "tags = " + str(tags) # is
-	print "data = " + str(data) # [444, 'stuff']
-	print "sour = " + str(source) # (network info)
+		closeOSC() # finally close the connection before exiting or program.	
 	
+def entry(addr, tags, data, source):
+	if (DEBUG):
+		print "entry handler called"
+		print "addr = " + str(addr) # /entry
+		print "tags = " + str(tags) # is
+		print "data = " + str(data) # [orbID, entryX, entryY, (R, G, B)]
+		print "sour = " + str(source) # (network info)
+	idx = data[0] % NUMBER_OF_LIGHTS
+	orbs[idx] = data # store orb data
+	canvas[idx] = data[3] # change pixel color to orb color
+	
+def exit(addr, tags, data, source):
+	if (DEBUG):
+		print "exit handler called"
+		print "addr = " + str(addr) # /exit
+		print "tags = " + str(tags) # is
+		print "data = " + str(data) # [orbID, entryX, entryY, (R, G, B)]]
+		print "sour = " + str(source) # (network info)
+	idx = data[0] % NUMBER_OF_LIGHTS
+	orbs[idx] = data
+	canvas[idx] = data[3]
+	
+def resetCanvas():
+	r = random() * 100
+	g = random() * 100
+	b = random() * 100
+	for i in NUMBER_OF_LIGHTS:
+		canvas[i] = (r,g,b)
+		
+def plasmaCanvas():
+	for i in NUMBER_OF_LIGHTS:
+		canvas[i] = canvas[i] * random * 2
+
 if __name__ == "__main__":
-	setup()
+	run()
 
 	# import time
     # out = FadeAnimation()
--- animations/fallingblocks-chroma-layer/test_client.py
+++ animations/fallingblocks-chroma-layer/test_client.py
@@ -3,9 +3,14 @@ from simpleOSC import initOSCClient, sendOSCMsg
 ip = "127.0.0.1"
 port = 9125
 
+NUMBER_OF_LIGHTS = 24 # currently 24, EOH 48
+DEFAULT_COLOR = [(25.0, 25.0, 25.0)] # dim white light
+canvas = DEFAULT_COLOR * NUMBER_OF_LIGHTS
+
 def test():
 	initOSCClient(ip, port)
-
-	sendOSCMsg("/collision", [444, "stuff"])
+	#sendOSCMsg("/collision", [444, "stuff"]))
+	canvas[0] = (1.0,2.0,3.0)
+	print canvas
 
 test()
