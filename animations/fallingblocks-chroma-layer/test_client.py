57dd2726119d6a27c3ed54fe943164ed84de8874
scowalt <srwalte2@illinois.edu>
2013-03-05 16:07:24 -0600

    Fixed proper /enter and /exit of orbs, plasma is less extreme

--- animations/fallingblocks-chroma-layer/test_client.py
+++ animations/fallingblocks-chroma-layer/test_client.py
@@ -1,16 +1,22 @@
+import sys
 from simpleOSC import initOSCClient, sendOSCMsg
 
 ip = "127.0.0.1"
-port = 9125
+port = 9199
 
 NUMBER_OF_LIGHTS = 24 # currently 24, EOH 48
 DEFAULT_COLOR = [(25.0, 25.0, 25.0)] # dim white light
 canvas = DEFAULT_COLOR * NUMBER_OF_LIGHTS
 
 def test():
+	WAIT = 0.25
+	import time
 	initOSCClient(ip, port)
-	#sendOSCMsg("/collision", [444, "stuff"]))
-	canvas[0] = (1.0,2.0,3.0)
-	print canvas
+	for i in range(0,50):
+		sendOSCMsg("/entry", [i,0,0] + [(1024,200,900)])
+		time.sleep(WAIT)
+	for i in range(0,50):
+		sendOSCMsg("/exit", [i,0,0] + [(0,0,0)])
+		time.sleep(WAIT)
 
 test()
