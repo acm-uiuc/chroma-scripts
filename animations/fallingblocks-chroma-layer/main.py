0ca947b18e3838e1e59e5c49f55c067b35d15816
scowalt <srwalte2@illinois.edu>
2013-03-06 22:47:44 -0600

    Changed OSCHandlers to handle /asteroid.

--- animations/fallingblocks-chroma-layer/main.py
+++ animations/fallingblocks-chroma-layer/main.py
@@ -1,100 +1,86 @@
+#################
+#	IMPORTS		#
+#################
 import sys
 from animations import FadeAnimation
 from simpleOSC import initOSCServer, startOSCServer, closeOSC, setOSCHandler
 import OSC
 import oscapi
-from random import random, randint
+from random import random, randint, shuffle
+import colorsys
 
-DEBUG = 1
-
+#################
+#	VARIABLES	#
+#################
+DEBUG = 1 # 0/1, gives more program feedback
 ip = "127.0.0.1" # receiving osc from 
 port = 9199
-MAX_COLOR = 1024
-NUMBER_OF_LIGHTS = 48 # working for 24, EOH 48
+MAX_COLOR = 1024 # max intensity of any color value
+NUMBER_OF_LIGHTS = 48
 default_color = [(102.4, 102.4, 102.4)] # dim white light
-canvas = default_color * NUMBER_OF_LIGHTS
-default_orb = [-1,-1,-1,-1,-1,-1,-1]
-orbs = [default_orb] * NUMBER_OF_LIGHTS # [orbID, entryX, entryY, R, G, B, canvasidx]
-out = FadeAnimation()
+canvas = default_color * NUMBER_OF_LIGHTS # this gets sent to chroma
+default_orb = [-1,-1,-1, -1] # [orbID, distance, hue, canvasID]
+orbs = [default_orb] * NUMBER_OF_LIGHTS # this stores orb data
+map_IDs = [-1] * NUMBER_OF_LIGHTS # maps from orbID to canvasID
+out = FadeAnimation() 
 
+#################
+#	MAIN		#
+#################
 def run():
 	# SETUP #
 	resetCanvas()
 	import time
-	out.FADERATE = 8 #fade rate, currently broken?
+	out.FADERATE = 8 # fade rate, currently broken?
 	out.start()
-	initOSCServer(ip, port) # start OSC server
 	
-	# OSC HANDLERS #
-	setOSCHandler('/entry', entry)	# when a new orb enters the screen
-	setOSCHandler('/exit', exit)	# when an orb exits the screen
+	# OSC SERVER/HANDLERS #
+	initOSCServer(ip, port) # setup OSC server
 	setOSCHandler('/reset', reset)	# resets the visualization
-	setOSCHandler('/move', move)	# when an orb moves on the screen
+	setOSCHandler('/orb', orb)	# when an orb moves on the screen
 
 	# SERVER START #
 	startOSCServer()
 	print "server is running, listening at " + str(ip) + ":" + str(port)
 
+	# SERVER LOOP#
 	try:
 		clock = 1
 		while(1):
-			out.write(canvas) #write to canvas
-			time.sleep(0.1)
-			if ((clock%1000) == 0):
+			out.write(canvas) # write to canvas
+			time.sleep(0.1) # tick every 0.1 seconds
+			if ((clock%100) == 0): # every 1000 ticks
 				plasmaCanvas()
-			clock = (clock+1)%10000 # reset clock every 10000 ticks
+			clock = (clock+1)%10000 # increment clock, reseting every 10000 ticks
 	except KeyboardInterrupt:
 		print "closing all OSC connections... and exit"
-		closeOSC() # finally close the connection before exiting or program.	
-	
-def entry(addr, tags, data, source):
+		closeOSC() # close the osc connection before exiting	
+
+
+#####################
+#	OSC HANDLERS	#
+#####################
+def orb(addr, tags, data, source):
 	if (DEBUG):
-		print "entry handler called"
-		print "addr = " + str(addr) # /entry
-		# print "tags = " + str(tags) # is
-		print "data = " + str(data) # [orbID, entryX, entryY, R, G, B]
-		# print "sour = " + str(source) # (network info)
-	idx = data[0] + randint(0,NUMBER_OF_LIGHTS)
-	idx = idx % NUMBER_OF_LIGHTS
-	orbs[idx] = data + [idx] # store orb data
-	pix = (data[3],data[4],data[5])
-	canvas[idx] = pix # change pixel color to orb color
-	
-def exit(addr, tags, data, source):
-	if (DEBUG):
-		print "exit handler called"
-		print "addr = " + str(addr) # /exit
-		# print "tags = " + str(tags) # is
-		print "data = " + str(data) # [orbID, exitX, exitY, R, G, B]
-		# print "sour = " + str(source) # (network info)
-		# print "orbs = " + str(orbs)
-	for i in range(0, NUMBER_OF_LIGHTS): # for all orb data
-		if (orbs[i][0] == data[0]): # if the orb is currently in the array
-			if (DEBUG):
-				print "found orb for removal: " + str(orbs[i])
-			canvas[orbs[i][6]] = default_color[0]	# reset the color of its light
-			orbs[i] = [-1,-1,-1,-1,-1,-1,-1] # reset the orb
-			
-def move(addr, tags, data, source):
-	if (DEBUG):
-		print "move handler called"
+		print "orb handler called"
 		print "addr = " + str(addr) # /move
 		# print "tags = " + str(tags) # is
-		print "data = " + str(data) # [orbID, exitX, exitY, R, G, B]
+		print "data = " + str(data) # [orbID, x, y, curvature, force, hue, distance, age]
 		# print "sour = " + str(source) # (network info)
 		# print "orbs = " + str(orbs)
-	for i in range(0, NUMBER_OF_LIGHTS): # for all orb data
-		if (orbs[i][0] == data[0]): # if the orb is currently in the array
-			if (DEBUG):
-				print "found orb for change: " + str(orbs[i])
-			canvas[orbs[i][6]] = (data[3], data[4], data[5]) # reset the pixel
-			orbs[i] = data + [orbs[i][6]] # reset the orb
-			if (DEBUG):
-				print "new orb: " + str(orbs[i])
-			
+	index		= data[0] % NUMBER_OF_LIGHTS
+	orbs[index]	= [data[0],data[6],data[5],map_IDs[index]] # [orbID, distance, hue, canvasID]
+	raw_color	= colorsys.hsv_to_rgb(data[5] / 360.0, 1, 1) #(h, s, v)
+	color		= oscapi.mult(raw_color, (MAX_COLOR / 2.0) + (MAX_COLOR / 2.0 * data[6])) # go from [0, 1) to [0, MAX_COLOR)
+	canvas[map_IDs[index]] = color
+		
 def reset(addr, tags, data, source):
 	resetCanvas()
-	
+
+
+#############################
+#	CANVAS MANIPULATION		#
+#############################
 def resetCanvas():
 	for i in range(0, NUMBER_OF_LIGHTS):
 		orbs[i] = default_orb
@@ -104,6 +90,9 @@ def resetCanvas():
 	default_color[0] = (r,g,b)
 	for i in range(0, NUMBER_OF_LIGHTS):
 		canvas[i] = (r,g,b)
+		map_IDs[i] = i
+	shuffle(map_IDs)
+	if (DEBUG): print "shuffle: " + str(map_IDs)
 	plasmaCanvas()
 		
 def plasmaCanvas():
@@ -111,23 +100,11 @@ def plasmaCanvas():
 		r = ((random()*0.1) + 0.95)
 		#if (DEBUG): print r
 		canvas[i] = oscapi.mult(canvas[i], r)
-	default_color[0] = oscapi.mult(default_color[0], ((random()*0.5) + 0.75))
+	shuffle(map_IDs)
+	
 
+#################
+#	???			#
+#################
 if __name__ == "__main__":
 	run()
-
-	# import time
-    # out = FadeAnimation()
-    # out.FADERATE = 8.0
-    # out.start()
-
-    # while True:
-        # for i in range(24):
-            # pix = [(0.0,0.0,0.0)]*24
-            # if i%8 == 0:
-                # pix[i] = (1023.0,0.0,0.0)
-            # else:
-                # pix[i] = (0.0,1023.0,1023.0)
-            # out.write(pix)
-            # time.sleep(0.7)
-            # time.sleep(0.2)
--- animations/fallingblocks-chroma-layer/test_client.py
+++ animations/fallingblocks-chroma-layer/test_client.py
@@ -9,18 +9,14 @@ DEFAULT_COLOR = [(25.0, 25.0, 25.0)] # dim white light
 canvas = DEFAULT_COLOR * NUMBER_OF_LIGHTS
 
 def test():
-	WAIT = 0.25
+	WAIT = 0.05
 	import time
 	initOSCClient(ip, port)
 	
-	#for i in range(0,50):
-	#	sendOSCMsg("/entry", [i,0,0] + [(1024,200,900)])
-	#	time.sleep(WAIT)
-	#for i in range(0,50):
-	#	sendOSCMsg("/exit", [i,0,0] + [(0,0,0)])
-	#	time.sleep(WAIT)
-
-	sendOSCMsg("/entry", [1,0,0] + [1024,200,100])
-	time.sleep(WAIT)
-	sendOSCMsg("/move", [1,23,255] + [700,100,200])
+	while (1):
+		for j in range(0,360):
+			for i in range(0,48):
+				sendOSCMsg("/orb", [i,0,0,0,0,j,1,0])
+		time.sleep(WAIT)
+	
 test()
