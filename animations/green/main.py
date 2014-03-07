d19b256e0c22695a3307db0799300aa42a29a5bb
Cole Gleason <colewashere@gmail.com>
2014-03-07 14:25:25 -0600

    green furnace

---
+++ animations/green/main.py
@@ -0,0 +1,29 @@
+import sys, random
+from oscapi import ColorsOut
+
+falloffRate = 7.0
+
+#author: Robert Pieta
+#modified by: Cole Gleason
+if __name__ == "__main__":
+    import time
+    out = ColorsOut()
+    pix = [(0.0,0.0,0.0)] * 48
+    runInAnimation = True
+    smoothnessRatio = 200
+    sleepTime = .01
+    while True:
+        if runInAnimation:
+            for i in xrange(12):
+                pix[random.randint(0,47)] = (0.0, pix[i][0] + 1023/smoothnessRatio, 0.0)
+            out.write(pix)
+            if pix[i][0] > 900:
+                    runInAnimation = False
+            time.sleep(sleepTime)
+        else:
+            for i in xrange(12):
+                pix[random.randint(0,47)] = (0.0, pix[i][0] - 1023/smoothnessRatio, 0.0)
+            out.write(pix)
+            if pix[i][0] < 100.0:
+                runInAnimation = True
+            time.sleep(sleepTime)
