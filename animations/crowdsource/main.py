8aab2071b835da5b9b6e8c3c6cd44001bd50e9c8
Milan <mdasgup2@illinois.edu>
2015-01-02 19:29:01 -0600

    Cleaner code and clear instructions for crowdsource

--- animations/crowdsource/main.py
+++ animations/crowdsource/main.py
@@ -7,7 +7,6 @@ import email
 import email.header
 import datetime
 import random
-from random import randint
 import time
 
 EMAIL_ACCOUNT = "lightschroma@gmail.com"
@@ -31,109 +30,65 @@ black,black,black,black,black,black,
 black,black,black,black,black,black,
 black,black,black,black,black,black]
 
+out = FadeAnimation()
+
+def pushcolor(color):
+    time.sleep(1.0)
+    pixels[0] = color
+    out.write(pixels)
+    time.sleep(1.0)
+    random.shuffle(pixels)
+    pixels[0] = black
+    out.write(pixels)
+
 if __name__ == "__main__":
-    password = "PASSWORD GOES HERE"
-    out = FadeAnimation()
+    password = "PASSWORD GOES HERE" 
     out.start()
     out.write(pixels)
     while True:
         M = imaplib.IMAP4_SSL('imap.gmail.com')
-
         try:
             rv, data = M.login(EMAIL_ACCOUNT, password)
         except imaplib.IMAP4.error:
             sys.exit(1)
-
-        print rv, data
-
         rv, mailboxes = M.list()
         rv, data = M.select(EMAIL_FOLDER)
-
         if rv == 'OK':
-    
             rv, data = M.search(None, "ALL")
             if rv != 'OK':
                 M.close()
                 sys.exit(1)
-    
+            numcolors = 0
             for num in data[0].split():
+                numcolors = numcolors + 1
                 rv, data = M.fetch(num, '(RFC822)')
                 if rv != 'OK':
                     M.close()
                     sys.exit(1)
-        
                 msg = email.message_from_string(data[0][1])
                 decode = email.header.decode_header(msg['Subject'])[0]
                 subject = unicode(decode[0])
                 if subject == "red":
-                    time.sleep(1.0)
-                    pixels[0] = red
-                    out.write(pixels)
-                    time.sleep(1.0)
-                    random.shuffle(pixels)
-                    pixels[0] = black
-                    out.write(pixels)
-                if subject == "blue":
-                    time.sleep(1.0)
-                    pixels[0] = blue
-                    out.write(pixels)
-                    time.sleep(1.0)
-                    random.shuffle(pixels)
-                    pixels[0] = black
-                    out.write(pixels)
-                if subject == "green":
-                    time.sleep(1.0)
-                    pixels[0] = green
-                    out.write(pixels)
-                    time.sleep(1.0)
-                    random.shuffle(pixels)
-                    pixels[0] = black
-                    out.write(pixels)
-                if subject == "black":
-                    time.sleep(1.0)
-                    pixels[0] = black
-                    out.write(pixels)
-                    time.sleep(1.0)
-                    random.shuffle(pixels)
-                    pixels[0] = black
-                    out.write(pixels)
-                if subject == "orange":
-                    time.sleep(1.0)
-                    pixels[0] = orange
-                    out.write(pixels)
-                    time.sleep(1.0)
-                    random.shuffle(pixels)
-                    pixels[0] = black
-                    out.write(pixels)
-                if subject == "yellow":
-                    time.sleep(1.0)
-                    pixels[0] = yellow
-                    out.write(pixels)
-                    time.sleep(1.0)
-                    random.shuffle(pixels)
-                    pixels[0] = black
-                    out.write(pixels)
-                if subject == "purple":
-                    time.sleep(1.0)
-                    pixels[0] = purple
-                    out.write(pixels)
-                    time.sleep(1.0)
-                    random.shuffle(pixels)
-                    pixels[0] = black
-                    out.write(pixels)
-                if subject == "teal":
-                    time.sleep(1.0)
-                    pixels[0] = teal
-                    out.write(pixels)
-                    time.sleep(1.0)
-                    random.shuffle(pixels)
-                    pixels[0] = black
-                    out.write(pixels)
-
+                    pushcolor(red)
+                elif subject == "blue":
+                    pushcolor(blue)
+                elif subject == "green":
+                    pushcolor(green)
+                elif subject == "black":
+                    pushcolor(black)
+                elif subject == "orange":
+                    pushcolor(orange)
+                elif subject == "yellow":
+                    pushcolor(yellow)
+                elif subject == "purple":
+                    pushcolor(purple)
+                elif subject == "teal":
+                    pushcolor(teal)
                 M.store(num, '+FLAGS', '\\Deleted')
+            if numcolors == 0:
+                time.sleep(1.0)
+                random.shuffle(pixels)
+                out.write(pixels)
             M.expunge()
             M.close()
-            time.sleep(1.0)
-            random.shuffle(pixels)
-            out.write(pixels)
         M.logout()
